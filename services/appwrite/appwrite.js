cae7154f77ddb65f4eb4cf6734838c3800e168fa
Eduardo <eduardogomez.sent2@gmail.com>
2024-03-20 15:00:16 -0500

    feat: add apwwrite for athentication and databases file algo appwrite dependency

---
+++ services/appwrite/appwrite.js
@@ -0,0 +1,12 @@
+import { Client, Account, Databases } from "appwrite";
+
+const client = new Client();
+
+client
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('65fb1b72d8f5ddc566e6');
+
+const account = new Account(client);
+const databases = new Databases(client);
+
+export {client, account, databases }
